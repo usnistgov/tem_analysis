31612530ce4a69d3e5675794073aa540c191f53c
Wesley Griffin <wesley.griffin@nist.gov>
2014-07-31 13:41:29 -0400

    fix compile on windows

--- sdk/compile.bat
+++ sdk/compile.bat
@@ -2,8 +2,8 @@
 
 "C:\Program Files\7-Zip\7z.exe" e InsightToolkit-4.5.0-nist.tar.gz
 "C:\Program Files\7-Zip\7z.exe" x InsightToolkit-4.5.0-nist.tar
-move InsightToolKit-4.5.0 ITK-4.5.0
-del InsightToolKit-4.5.0-nist.tar
+move InsightToolkit-4.5.0 ITK-4.5.0
+del InsightToolkit-4.5.0-nist.tar
 
 cd ITK-4.5.0
 mkdir build
@@ -15,7 +15,7 @@ cd ..\..
 
 "C:\Program Files\7-Zip\7z.exe" e VTK-6.1.0-nist.tar.gz
 "C:\Program Files\7-Zip\7z.exe" x VTK-6.1.0-nist.tar
-copy /Y VTK-6.0.1-CMakeLists-windows.txt VTK-6.1.0\CMakeLists.txt
+copy /Y VTK-6.1.0-CMakeLists-windows.txt VTK-6.1.0\CMakeLists.txt
 del VTK-6.1.0-nist.tar
 
 cd VTK-6.1.0
